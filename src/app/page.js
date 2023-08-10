f300b3d918ae2092246a7c9b5861f3f6053cd4f1
Tanapat Choeichomsri <tanapat_choeichomsri@cmu.ac.th>
2023-08-10 14:46:52 +0700

    Test 2

--- src/app/page.js
+++ src/app/page.js
@@ -14,56 +14,13 @@ export default function HomePage() {
         style={{ maxWidth: "700px" }}
         className="mx-auto p-3 rounded rounded-3 shadow-sm bg-white"
       >
-        {/* Post Owner Example*/}
-        {/* <div className="vstack gap-3">
-          <div className="d-flex align-items-center gap-3">
-            <img
-              src="/profileImages/FB_Profile.jpg"
-              width="48"
-              height="48"
-              className="rounded-circle"
-              style={{ objectFit: "cover" }}
-            />
-            <span className="fw-semibold fs-5">Tanapat Choeichomsri 650610767</span>
-          </div>
-
-          <span>Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207</span>
-
-          <div className="d-flex align-items-center gap-1">
-            <img src="/like.svg" width={20}></img>
-            <span className="text-muted">100 คน</span>
-          </div>
-          <hr className="m-0 border" />
-        </div> */}
         <PostOwner
-          OwnerImagePath ="/profileImages/FB_Profile.jpg" 
-          OwnerName="Tanapat Choeichomsri 650610767"
-          OwnerText="ทำ Quiz จนท้อแล้วครับ ได้โปรดปราณีด้วย"
-          OwnerLikeNum="100 คน"
+          OwnerImagePath ={/profileImages/FB_Profile.jpg} 
+          OwnerName={"Tanapat Choeichomsri 650610767"}
+          OwnerText={"ทำ Quiz จนท้อแล้วครับ ได้โปรดปราณีด้วย"}
+          OwnerLikeNum={"100 คน"}
         />
 
-        {/* Comment Example */}
-        {/* <div className="d-flex gap-2 my-2">
-          <img
-            src="/profileImages/lisa.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
-          />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#E5E7EB" }}
-          >
-            <span className="fw-semibold">Lisa</span>
-            <br />
-            <span>จริงค่า</span>
-            <div className="d-flex align-items-center gap-1">
-              <img src="/like.svg" width={20}></img>
-              <span className="text-muted">999 คน</span>
-            </div>
-          </div>
-        </div> */}
         <Comment
           userImagePath ="comments.userImagePath"
           username="comments.username"
@@ -71,29 +28,15 @@ export default function HomePage() {
           likeNum="comments.likeNum"
           replies="comments.replies"
         />
-        {/* Reply Example */}
-        <div className="d-flex gap-2 my-2 ps-5">
-          <img
-            src="/profileImages/puppy.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
+<div>{Comment.map((comments) => (
+          <Comment
+            userImagePath={comments.userImagePath}
+            username={comments.username}
+            replyText={comments.replyText}
+            likeNum={comments.likeNum}
+            replies={comments.replies}
           />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#E5E7EB" }}
-          >
-            <span className="fw-semibold">หมาน้อย</span>
-            <br />
-            <span>จริงค้าบบบบบบบบ</span>
-            <div className="d-flex align-items-center gap-1">
-              <img src="/like.svg" width={20}></img>
-              <span className="text-muted">2 คน</span>
-            </div>
-          </div>
-        </div>
-
+        ))}</div>
         {/* map-loop render Comment component here */}
       </div>
     </div>
