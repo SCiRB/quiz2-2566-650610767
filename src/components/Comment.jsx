7e3a97e602e474fede59e1a79ab3173a5be717aa
Tanapat Choeichomsri <tanapat_choeichomsri@cmu.ac.th>
2023-08-10 15:30:41 +0700

    I hate this

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -11,16 +11,17 @@ export const Comment = ({
 }) => {
   return (
     <div className="d-flex gap-2 my-2">
-      <img src ={userImagePath} width="48" height="48" className="rounded-circle"style={{ objectFit: "cover" }}/>
+      <img src ={userImagePath} width="48" height="48" className="rounded-circle"style={{ objectFit: "cover" }}></img>
       <div className="rounded rounded-3 p-2" style={{ backgroundColor: "#E5E7EB" }}>
         <span className="fw-semibold"> {username} </span>
         <br/>
         <span>{commentText}</span>
         <div className="d-flex align-items-center gap-1">
         <img src="/like.svg" width={20}></img>
-        <span className="text-muted">{likeNum}</span>
+        <span className="text-muted">{likeNum} คน</span>
         </div>
-        {replies.map((replies) => (
+      
+      <div>{replies.map((replies) => (
           <Reply
             userImagePath={replies.userImagePath}
             username={replies.username}
@@ -28,6 +29,7 @@ export const Comment = ({
             likeNum={replies.likeNum}
           />
         ))}
+      </div>
       </div>
       {/* map-loop render Reply component here */}
     </div>
--- src/components/Reply.jsx
+++ src/components/Reply.jsx
@@ -4,11 +4,12 @@ export const Reply = ({ userImagePath, username, replyText, likeNum }) => {
     <img src ={userImagePath} width="48" height="48" className="rounded-circle"style={{ objectFit: "cover" }}></img>
     <div className="rounded rounded-3 p-2" style={{ backgroundColor: "#E5E7EB" }}>
       <span className="fw-semibold"> {username} </span>
+      <br/>
       <span>{replyText}</span>
-    </div>
     <div className="d-flex align-items-center gap-1">
       <img src="/like.svg" width={20}></img>
-      <span className="text-muted">{likeNum}</span>
+      <span className="text-muted">{likeNum} คน</span>
+    </div>
     </div>
   </div>);
 };
